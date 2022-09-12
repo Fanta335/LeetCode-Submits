a565ada7097b18fac75c2670e092ad14547cd0e0
Fanta335 <fantayusuke5210@gmail.com>
2022-09-12 11:03:32 +0900

    Submit 162.

---
+++ problems/0162/162.find-peak-element.java
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=162 lang=java
+ *
+ * [162] Find Peak Element
+ */
+
+// @lc code=start
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int l = 0;
+        int r = nums.length - 1;
+        while (l < r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] < nums[mid + 1]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return l;
+    }
+}
+// @lc code=end
+
