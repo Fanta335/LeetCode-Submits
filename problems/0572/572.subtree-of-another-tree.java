3750446f39db800c75b545afd8deae6f652a3883
Fanta335 <fantayusuke5210@gmail.com>
2022-09-17 13:58:17 +0900

    Submit 572.

---
+++ problems/0572/572.subtree-of-another-tree.java
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=572 lang=java
+ *
+ * [572] Subtree of Another Tree
+ */
+
+// @lc code=start
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (isIdentical(root, subRoot)) {
+            return true;
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean isIdentical(TreeNode s, TreeNode t) {
+        if (s == null && t == null) {
+            return true;
+        }
+        if (s == null || t == null) {
+            return false;
+        }
+        if (s.val == t.val) {
+            return isIdentical(s.left, t.left) && isIdentical(s.right, t.right);
+        }
+        return false;
+    }
+}
+// @lc code=end
+
