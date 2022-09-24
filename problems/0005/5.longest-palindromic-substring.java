4f52fffae116f8873156dc113791954d38cc2ff8
Fanta335 <fantayusuke5210@gmail.com>
2022-09-24 10:22:42 +0900

    Submit 5.

---
+++ problems/0005/5.longest-palindromic-substring.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=5 lang=java
+ *
+ * [5] Longest Palindromic Substring
+ */
+
+// @lc code=start
+class Solution {
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int odd = expand(s, i, i);
+            int even = expand(s, i, i + 1);
+            int longer = odd > even ? odd : even;
+            if (longer > end - start) {
+                start = i - (longer - 1) / 2;
+                end = i + longer / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private int expand(String s, int l, int r) {
+        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+            l--;
+            r++;
+        }
+        return r - l - 1;
+    }
+}
+// @lc code=end
+
