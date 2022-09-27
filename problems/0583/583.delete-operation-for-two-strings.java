b052559bdfcd6a0b17dae2629b6733febd985c3c
Fanta335 <fantayusuke5210@gmail.com>
2022-09-27 13:25:24 +0900

    Submit 583.

---
+++ problems/0583/583.delete-operation-for-two-strings.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=583 lang=java
+ *
+ * [583] Delete Operation for Two Strings
+ */
+
+// @lc code=start
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length();
+        int n = word2.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return (m + n) - (dp[m][n] * 2);
+    }
+}
+// @lc code=end
+
