42f37698a3268895fdac6736f9a3c51b7976e893
Fanta335 <fantayusuke5210@gmail.com>
2022-09-28 19:36:20 +0900

    Submit 139.

---
+++ problems/0139/139.word-break.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=139 lang=java
+ *
+ * [139] Word Break
+ */
+
+// @lc code=start
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        boolean[] f = new boolean[s.length() + 1];
+        f[0] = true;
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (f[j] && wordDict.contains(s.substring(j, i))) {
+                    f[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return f[s.length()];
+    }
+}
+// @lc code=end
+
