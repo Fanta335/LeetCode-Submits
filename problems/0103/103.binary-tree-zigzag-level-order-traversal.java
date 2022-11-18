574cae875d1b785f2b6624a175db79a30dcd1ede
Fanta335 <fantayusuke5210@gmail.com>
2022-11-18 17:51:41 +0900

    Submit 103.

---
+++ problems/0103/103.binary-tree-zigzag-level-order-traversal.java
@@ -0,0 +1,55 @@
+import java.util.LinkedList;
+import java.util.List;
+
+/*
+ * @lc app=leetcode id=103 lang=java
+ *
+ * [103] Binary Tree Zigzag Level Order Traversal
+ */
+
+// @lc code=start
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {}
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> res = new LinkedList<>();
+        helper(root, res, 0);
+        return res;
+    }
+
+    private void helper(TreeNode node, List<List<Integer>> list, int level) {
+        if (node == null) {
+            return;
+        }
+        if (list.size() < level + 1) {
+            list.add(new LinkedList<>());
+        }
+        if (level % 2 == 0) {
+            list.get(level).add(node.val);
+        } else {
+            ((LinkedList<Integer>) list.get(level)).addFirst(node.val);
+        }
+        helper(node.left, list, level + 1);
+        helper(node.right, list, level + 1);
+    }
+}
+// @lc code=end
+
