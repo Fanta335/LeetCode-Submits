b8e21b531d8b266ad4dec5ba21ecf8d82b81cd6e
Fanta335 <fantayusuke5210@gmail.com>
2022-12-01 17:09:51 +0900

    Submit 153.

--- problems/0153/153.find-minimum-in-rotated-sorted-array.java
+++ problems/0153/153.find-minimum-in-rotated-sorted-array.java
@@ -10,14 +10,14 @@ class Solution {
         int l = 0;
         int r = nums.length - 1;
         while (l < r) {
-            int mid = (r - l) / 2 + l;
-            if (nums[l] < nums[r]) {
-                return nums[l];
+            if (nums[l] <= nums[r]) {
+                break;
             }
-            if (nums[mid] < nums[r]) {
-                r = mid;
-            } else {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] >= nums[l]) {
                 l = mid + 1;
+            } else {
+                r = mid;
             }
         }
         return nums[l];
