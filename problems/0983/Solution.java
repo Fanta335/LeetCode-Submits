100675c05ebf708e19e0cc7916a1e50e332a5eb9
Fanta335 <fantayusuke5210@gmail.com>
2023-03-28 16:22:25 +0900

    submit 983

---
+++ problems/0983/Solution.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+  public int mincostTickets(int[] days, int[] costs) {
+    Set<Integer> daySet = new HashSet<>();
+    for (int d : days) {
+      daySet.add(d);
+    }
+    int[] dp = new int[366];
+    for (int i = 1; i < 366; i++) {
+      if (!daySet.contains(i)) {
+        dp[i] = dp[i - 1];
+      } else {
+        dp[i] = Math.min(dp[i - 1] + costs[0],
+            Math.min(dp[Math.max(0, i - 7)] + costs[1], dp[Math.max(0, i - 30)] + costs[2]));
+      }
+    }
+    return dp[365];
+  }
+}
