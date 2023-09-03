1e58b763ad87272f66a59415b7e99fdfa487ace8
Fanta335 <fantayusuke5210@gmail.com>
2023-09-03 19:07:34 +0900

    submit 62

---
+++ problems/0062/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int uniquePaths(int m, int n) {
+    int[][] dp = new int[m + 1][n + 1];
+    dp[1][1] = 1;
+    for (int y = 1; y <= m; y++) {
+      for (int x = 1; x <= n; x++) {
+        dp[y][x] += dp[y - 1][x] + dp[y][x - 1];
+      }
+    }
+
+    return dp[m][n];
+  }
+}
