ba67e91e133ca30635334554cdc101df049941ef
Fanta335 <fantayusuke5210@gmail.com>
2023-10-03 10:18:21 +0900

    submit 1512

---
+++ problems/1512/1512.java
@@ -0,0 +1,13 @@
+class Solution {
+  public int numIdenticalPairs(int[] nums) {
+    int res = 0;
+    int[] counts = new int[101];
+
+    for (int num : nums) {
+      res += counts[num];
+      counts[num]++;
+    }
+
+    return res;
+  }
+}
